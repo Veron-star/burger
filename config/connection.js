46d7a1c8401be34c22aaa9b29a766ed42a57269f
veronica lim <veron_yan@yahoo.com.sg>
2020-08-06 14:35:18 +0800

    created connection.js and orm.js

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.veron_url) {
+    connection = mysql.createConnection(process.env.veron_url);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "happy",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
